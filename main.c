7a429870baf5c3bc9b1a3af633e587654d70b400
Jorge Varea Duran <jorvarea@c3r4s5.42malaga.com>
2023-11-14 22:07:09 +0100

    fix putstr

--- main.c
+++ main.c
@@ -6,14 +6,20 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 20:04:51 by jorvarea          #+#    #+#             */
-/*   Updated: 2023/11/14 20:04:52 by jorvarea         ###   ########.fr       */
+/*   Updated: 2023/11/14 21:06:52 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "stdio.h"
 
 int	main(void)
 {
-	ft_printf("HOLA %s", "caracola");
+    int a;
+    int b;
+    
+    a = printf(" NULL %s NULL \n", NULL);
+	b = ft_printf(" NULL %s NULL \n", NULL);
+    printf("a, b: %d, %d", a, b);
 	return (0);
 }
